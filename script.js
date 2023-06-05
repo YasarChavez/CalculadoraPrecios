14f7e629c288d18d1318cfd3442be5ddcdac3ba7
YasarChavez <tneniitoo@gmail.com>
2023-06-05 12:03:38 -0300

    Update

--- script.js
+++ script.js
@@ -1,71 +1,18 @@
-function openTab(evt, tabName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
-  }
-  
-  function calcularTotal() {
-    var kilo = parseFloat(document.getElementById('kilo').value);
-    var peso = parseFloat(document.getElementById('peso').value);
-    var total = kilo * (peso / 1000);
-    document.getElementById('total').textContent = 'Total: ' + total.toFixed(2);
+function calcularTotal() {
+    var valorKilo = document.getElementById("valorKilo").value;
+    var pesoTotal = document.getElementById("pesoTotal").value;
+    
+    var total = valorKilo * pesoTotal;
+    
+    document.getElementById("resultadoTotal").innerHTML = "El total es: " + total;
   }
   
   function calcularCantidad() {
-    var kilo = parseFloat(document.getElementById('kilo2').value);
-    var valor = parseFloat(document.getElementById('valor').value);
-    var cantidad = valor / kilo;
-    document.getElementById('cantidad').textContent = 'Cantidad: ' + cantidad.toFixed(2) + ' kg (o ' + (cantidad * 1000).toFixed(2) + ' g)';
+    var valorKilo = document.getElementById("valorKilo2").value;
+    var valorTotal = document.getElementById("valorTotal").value;
+    
+    var cantidad = valorTotal / valorKilo;
+    
+    document.getElementById("resultadoCantidad").innerHTML = "La cantidad a entregar es: " + cantidad + " kilogramos o gramos";
   }
-  
-  function limpiarCalculadora() {
-    var inputs = document.getElementsByTagName("input");
-    for (var i = 0; i < inputs.length; i++) {
-      inputs[i].value = "";
-    }
-    document.getElementById('total').textContent = "";
-    document.getElementById('cantidad').textContent = "";
-  }
-  
-  // Abrir la pestaña 'totalTab' por defecto al cargar la página
-  document.getElementById('totalTab').style.display = "block";
-  document.getElementsByClassName('tablinks')[0].className += " active";
-  
-  // Actualizar el tamaño de las pestañas al cambiar el tamaño de la ventana
-  window.addEventListener('resize', function() {
-    openTab(event, document.getElementsByClassName('active')[0].id);
-  });
-  
-  // Desactivar reinicio automático de la página al abrir el teclado en dispositivos móviles
-  var focusedElementBeforeOpen;
-  var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-  
-  window.addEventListener('load', function() {
-    document.addEventListener('focusin', function(event) {
-      focusedElementBeforeOpen = event.target;
-    });
-  
-    document.addEventListener('focusout', function() {
-      focusedElementBeforeOpen = null;
-    });
-  
-    window.addEventListener('resize', function() {
-      if (isMobile && document.activeElement === focusedElementBeforeOpen) {
-        setTimeout(function() {
-          focusedElementBeforeOpen.scrollIntoView({
-            behavior: 'smooth',
-            block: 'center',
-          });
-        }, 100);
-      }
-    });
-  });
   
